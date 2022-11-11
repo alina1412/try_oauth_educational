9721c1687b98b4f0365a6239e34ceab2421a5060
alina1412 <alina1412@mail.ru>
2022-11-11 18:51:35 +0300

    refactor

--- service/oauth/tokens.py
+++ service/oauth/tokens.py
@@ -1,20 +1,23 @@
 import datetime
-from typing import Any
 
-import jose
 from jose import jwt
 
 
-def generate_token(username: str, key: str, algorithm: str = "HS256"):
-    """ """
+def get_data_for_token(username: str) -> dict[str, str]:
     expire = datetime.datetime.utcnow() + datetime.timedelta(minutes=30)
     expire = expire.isoformat()
     data_to_encode = {"username": username, "expire": expire}
+    return data_to_encode
+
+
+def generate_token(username: str, key: str, algorithm: str = "HS256"):
+    """make token from username,  key with expiration in timedelta(minutes=30)"""
+    data_to_encode = get_data_for_token(username)
     token = jwt.encode(claims=data_to_encode, key=key, algorithm=algorithm)
     return token
 
 
-def decode_token(token: str, key: str) -> dict[str, Any]:
+def decode_token(token: str, key: str) -> dict[str, str]:
+    """data: # {"username": username, "expire": expire}"""
     data = jwt.decode(token, key)
-    # {"username": username, "expire": expire}
     return data
--- service/utils/utils.py
+++ service/utils/utils.py
@@ -1,16 +1,17 @@
 import datetime
+from typing import Optional
 
 from service.exceptions.exceptions import CredentialsException
 
 
-def verify_password(user, password) -> bool:
+def verify_password(user: Optional[dict], password) -> bool:
     password_in_db = user.get("password")
     if password_in_db == password:
         return True
     return False
 
 
-def verify_user(user: dict, password: str) -> bool:
+def verify_user(user: Optional[dict], password: str) -> bool:
     if not user:
         return False
     if not verify_password(user, password):
@@ -18,7 +19,7 @@ def verify_user(user: dict, password: str) -> bool:
     return True
 
 
-def validate_token(user, expired_time) -> None:
+def validate_token(user: Optional[dict], expired_time: str) -> None:
     if (
         not user
         or expired_time is None
